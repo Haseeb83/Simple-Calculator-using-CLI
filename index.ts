64a64c88e22a53a219f38a080cbc330fc5410b44
Haseeb Mustafa <haseb842@gmail.com>
2024-03-29 17:05:06 +0500

    Add files via upload
    Simple calculator using CLI

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([
+  { message: "Enter first Number", type: "number", name: "firstnumber" },
+  {message: "Enter second Number", type: "number", name: "secondnumber"},
+  {message:"select one of the operators to perform operations",
+  type: "list", name: "operator",
+  choices:["addition", "subtraction", "multiplication", "division"],
+},
+]);
+// conditional statement
+if(answer.operator==="addition"){
+console.log(answer. firstnumber + answer.secondnumber);
+} else if (answer.operator==="subtraction"){
+  console.log(answer. firstnumber - answer.secondnumber);
+} else if (answer.operator==="multiplication"){
+  console.log(answer. firstnumber * answer.secondnumber);
+} else if (answer.operator==="division"){
+  console.log(answer. firstnumber / answer.secondnumber);
+} else {
+  console.log ("Please select valid operator")
+}
